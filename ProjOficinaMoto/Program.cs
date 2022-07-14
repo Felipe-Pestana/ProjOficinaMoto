e7e235070124efccf543cf78f93fb8385a596215
Felipe Pestana <pestana@sitic.com.br>
2022-07-14 17:20:09 -0300

    Adicionar arquivos de projeto.

---
+++ ProjOficinaMoto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjOficinaMoto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
